013e923cd05d130ca1bc5c9acbf410243cc5e348
AbhiDaGreat <abhishekkumarmahato70@gmail.com>
2025-10-29 14:22:02 +0530

    Patch update 2.0

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
    await Listing.deleteMany({});
+   initData.data = initData.data.map((obj) => ({...obj, owner : '6811db8ad3ce68a358193814'}));
    await Listing.insertMany(initData.data);
    console.log("data was initialized");
 }
--- routes/listing.js
+++ routes/listing.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Listing = require("../models/listing.js");
+const {isLoggedIn, isOwner} = require("../middleware.js");
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 // Index Route (Show all listings)
 router.get("/", async (req, res) => {
@@ -11,42 +14,65 @@ router.get("/", async (req, res) => {
 });
 
 // New Route (Form)
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn , (req, res) => {
   res.render("listings/new.ejs");
 });
 
+
 // Create Route (Add listing to DB)
-router.post("/", wrapAsync (async (req, res, next) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
+router.post("/", isLoggedIn, wrapAsync(async (req, res, next) => {
+  const listingData = req.body.listing;
+
+  // Set default image if none provided
+  if (!listingData.image || !listingData.image.url) {
+      listingData.image = {
+          filename: "default",
+          url: "https://images.unsplash.com/photo-1506744038136-46273834b3fb"
+      };
+  }
+
+  const newListing = new Listing(listingData);
+  newListing.owner = req.user._id;
+  await newListing.save();
+  req.flash("success", "New listing created!");
+  res.redirect("/listings");
 }));
 
 // Show Route (Details page)
 router.get("/:id", async (req, res) => {
   let { id } = req.params;
-  const listing = await Listing.findById(id).populate("reviews");
+  const listing = await Listing.findById(id).populate({path : "reviews", populate : {path : "author"}}).populate("owner");
+  if(!listing){
+    req.flash("error", "Listing does not exist!");
+    res.redirect("/listings");
+  }
   res.render("listings/show.ejs", { listing });
 });
 
 // Edit Route (Form to edit)
-router.get("/:id/edit", async (req, res) => {
+router.get("/:id/edit", isLoggedIn, isOwner, async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
+  if(!listing){
+    req.flash("error", "Listing does not exist!");
+    res.redirect("/listings");
+  }
   res.render("listings/edit.ejs", { listing });
 });
 
 // Update Route (PUT)
-router.put("/:id", async (req, res) => {
+router.put("/:id", isLoggedIn, isOwner, async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  req.flash("success", "Listing Updated!");
   res.redirect(`/listings/${id}`);
 });
 
 // Delete Route
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", isLoggedIn, isOwner, async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
   });
 
